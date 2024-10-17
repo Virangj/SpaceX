fb962f5a5f356e5588e97be357e6b428ed5f49bb
Virang Jain <virangjain355@gmail.com>
2024-10-17 21:51:33 +0530

    Added Upcoming Events

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { BackgroundLinesDemo } from "./Components/Background";
+import { FocusCardsDemo } from "./Components/FocusCard";
 import { NavbarDemo } from "./Components/Navbar";
 import { StickyScrollRevealDemo } from "./Components/Sticky_Scroll";
 import { TimelineDemo } from "./Components/TimeLine";
@@ -10,6 +11,10 @@ export default function (){
       <BackgroundLinesDemo />
       <StickyScrollRevealDemo />
       <TimelineDemo />
+      <div className="bg-black py-20">
+        <h1 className="text-slate-500 font-bold flex justify-start pl-[25%] pb-20 text-7xl">Upcoming Events</h1>
+        <FocusCardsDemo />
+      </div>
     </div>
   )
 }
