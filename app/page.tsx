21b7c5735bb711e5e79972f862b2ac895156cdac
Virang Jain <virangjain355@gmail.com>
2025-04-24 08:35:18 +0530

    solving production errors 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { BackgroundLinesDemo } from "./Components/Background";
 import { FocusCardsDemo } from "./Components/FocusCard";
-// import { NavbarDemo } from "./Components/Navbar";
 import { StickyScrollRevealDemo } from "./Components/Sticky_Scroll";
 import { AnimatedTestimonialsDemo } from "./Components/Testimonials";
 import { TimelineDemo } from "./Components/TimeLine";
@@ -8,13 +7,13 @@ import { TimelineDemo } from "./Components/TimeLine";
 export default function (){
   return (
       <div className="min-h-screen overflow-x-hidden scrollbar-hide">
-      {/* <NavbarDemo /> */}
       <BackgroundLinesDemo />
       <StickyScrollRevealDemo />
       <TimelineDemo />
       <div className="bg-black py-20">
         <h1 className="text-slate-500 font-bold flex justify-start pl-[25%] pb-20 text-7xl">Upcoming Events</h1>
         <FocusCardsDemo />
+        <h1 className="text-slate-500 font-bold flex justify-start pl-[25%] pb-20 text-7xl pt-20">Testimonials</h1>
         <AnimatedTestimonialsDemo />
       </div>
     </div>
